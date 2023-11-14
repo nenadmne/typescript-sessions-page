28d9b132e9d9ffe9a314f9130bd0a0ec5b5343f6
nenadmne <cosovicnenad14@gmail.com>
2023-11-14 23:19:32 +0100

    SessionItem -added tailwind classes / wide screen

--- src/components/pages/sessions/components/SessionItem.tsx
+++ src/components/pages/sessions/components/SessionItem.tsx
@@ -12,14 +12,14 @@ type SessionItemProps = {
 export default function SessionItem({ item }: SessionItemProps) {
   const { image, title, summary, id } = item;
   return (
-    <article className="session-item">
-      <img src={image} alt={title} />
-      <div className="session-data">
+    <article className="flex flex-col bg-[#352e41] text-white rounded-md overflow-hidden shadow-shadow">
+      <img src={image} alt={title} className="w-full h-48 object-cover" />
+      <div className="flex flex-col p-4 justify-between">
         <div>
-          <h3>{title}</h3>
-          <p>{summary}</p>
+          <h3 className="mb-1 text-light-purple text-sm">{title}</h3>
+          <p className="mb-1 text-lighter-purple text-base">{summary}</p>
         </div>
-        <p className="actions">
+        <p className="mt-8 mb-4">
           <Link to={id}>Learn More</Link>
         </p>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        shadow: "0 2px 8px rgba(0, 0, 0, 0.26)",
+      },
+      colors: {
+        "light-purple": "#c7b0ee",
+        "lighter-purple": "#eae3f6",
+      },
+      fontSize: {
+        sm: "0.85rem",
+        base: "1rem",
+        xl: "1.25rem",
+        "2xl": "2rem",
+        "3xl": "3rem",
+        "4xl": "4rem",
+        "5xl": "5rem",
+      },
+    },
   },
   plugins: [],
 };
