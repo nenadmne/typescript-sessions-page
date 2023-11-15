192487021ed5f490c3144d491711bd2b9371da5a
nenadmne <cosovicnenad14@gmail.com>
2023-11-15 22:50:43 +0100

    Session component responsive design for all screens

--- src/pages/Session.tsx
+++ src/pages/Session.tsx
@@ -20,7 +20,7 @@ export default function SessionPage() {
   }
 
   return (
-    <main className="w-[80%] max-w-[60rem] my-12 mx-auto">
+    <main className="w-full lg:w-[80%] max-w-[60rem] mt-28 mx-auto">
       {openModal && (
         <BookSession
           session={loadedSession}
@@ -28,14 +28,14 @@ export default function SessionPage() {
         />
       )}
       <article>
-        <header className="flex gap-6">
+        <header className="flex flex-col sm:flex-row justify-between sm:gap-4">
           <img
             src={loadedSession.image}
             alt={loadedSession.title}
-            className="w-[30rem] h-80 object-cover rounded-lg mb-8"
+            className="flex w-[30rem] sm:w-[25rem] lg:w-[30rem] h-80 sm:h-52 md:h-64 lg:h-80 object-cover rounded-lg mb-4 sm:mb-0"
           />
-          <div>
-            <h2 className="mb-2 text-xxl text-session-header">
+          <div className="flex flex-col w-[50%] lg:w-[28rem]">
+            <h2 className="mb-2 text-l md:text-xl lg:xxl text-session-header">
               {loadedSession.title}
             </h2>
             <time
@@ -53,7 +53,7 @@ export default function SessionPage() {
             </p>
           </div>
         </header>
-        <p className="max-w-[60rem] my-8 mx-auto p-4 bg-grey rounded-lg shadow-shadow text-session-header text-l whitespace-pre-wrap animate-slide-down">
+        <p className="max-w-[60rem] my-8 mx-auto p-4 bg-grey rounded-lg shadow-shadow text-session-header text-base lg:text-l whitespace-pre-wrap animate-slide-down">
           {loadedSession.description}
         </p>
       </article>
